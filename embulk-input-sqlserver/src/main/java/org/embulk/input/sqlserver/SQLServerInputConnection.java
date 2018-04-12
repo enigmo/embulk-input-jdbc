fb60c8e2bccc57132023126526dcf345105dc432
Shintaro Kimura <kimura@enigmo.co.jp>
2018-04-12 10:20:20 +0900

    Add nolock table hint into select query.

--- embulk-input-sqlserver/src/main/java/org/embulk/input/sqlserver/SQLServerInputConnection.java
+++ embulk-input-sqlserver/src/main/java/org/embulk/input/sqlserver/SQLServerInputConnection.java
@@ -2,6 +2,8 @@ package org.embulk.input.sqlserver;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import com.google.common.base.Optional;
 import org.embulk.input.jdbc.JdbcInputConnection;
 
 public class SQLServerInputConnection extends JdbcInputConnection {
@@ -25,6 +27,29 @@ public class SQLServerInputConnection extends JdbcInputConnection {
             sb.append(quoteIdentifierString(schemaName)).append(".");
         }
         sb.append(quoteIdentifierString(tableName));
+        return sb.toString();
+    }
+
+    @Override
+    public String buildSelectQuery(String tableName,
+                                   Optional<String> selectExpression, Optional<String> whereCondition,
+                                   Optional<String> orderByExpression) throws SQLException
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("SELECT ");
+        sb.append(selectExpression.or("*"));
+        sb.append(" FROM ").append(buildTableName(tableName));
+        sb.append("(nolock)");
+
+        if (whereCondition.isPresent()) {
+            sb.append(" WHERE ").append(whereCondition.get());
+        }
+
+        if (orderByExpression.isPresent()) {
+            sb.append(" ORDER BY ").append(orderByExpression.get());
+        }
+
         return sb.toString();
     }
 
